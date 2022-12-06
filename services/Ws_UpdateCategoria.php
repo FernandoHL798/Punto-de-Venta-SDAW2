dcc724aa631a51d67c2f43602dee8f657d6f3460
FernandoHL798 <lfernandohl901@gmail.com>
2022-12-06 12:13:35 -0600

    Pantalla categorias completo

--- services/Ws_UpdateCategoria.php
+++ services/Ws_UpdateCategoria.php
@@ -1,17 +1,23 @@
 <?php
-require_once "../control/controlCategoria.php";
-/**
- * Variables que se van a usar  que llegan de Front o del API
- */
-$idCategoria = "4";
-$nombreCategoria = "Lavanderia";
 
-if(editaCategoria($idCategoria,$nombreCategoria)){
-    $mjeType =  "1";
-    $mjeText =  "Se ha actualizado correctamente";
+if(isset($_POST['idCategoria']) && isset($_POST['nombreCategoria'])){
+    require_once "../control/controlCategorias.php";
+    /**
+     * Variables que se van a usar  que llegan de Front o del API
+     */
+    $idCategoria = $_POST['idCategoria'];
+    $nombreCategoria = $_POST['nombreCategoria'];
+
+    if(editaCategoria($idCategoria,$nombreCategoria)){
+        $mjeType =  "1";
+        $mjeText =  "Se ha actualizado correctamente";
+    } else{
+        $mjeType =  "-1";
+        $mjeText =  "Hubo un error interno";
+    }    
 } else{
     $mjeType =  "0";
-    $mjeText =  "Hubo un error interno";
+        $mjeText =  "Faltan datos";
 }
 
 $mje = array(
