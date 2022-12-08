8cb100338d392164dc2927d79c37c5114018bd9d
Cristian-RC <cristian.rubio@comunidad.unam.mx>
2022-12-08 00:27:23 -0600

    control usuarios

--- control/controlUsuarios.php
+++ control/controlUsuarios.php
@@ -9,8 +9,12 @@ function consultaUsuario($username,$pw){
 
 //Buscar el correo electronico entrante y pw dentro De NUESTRA BASE DE DATOS
 function autorizaAccesoAlmacen($correo,$pw){
+    include_once "../model/Usuario.php";
+    $user = new Usuario();
     if (buscarUsuarioAPI()){
         //buscar el usuario en la BD
+        $user->setEmail($correo);
+        $user->setPassword($pw);
         return true;
     }
     return false;
@@ -18,6 +22,10 @@ function autorizaAccesoAlmacen($correo,$pw){
 
 //Implementada por Chris R.
 function buscarUsuarioAPI($correo,$pw){
+    include_once "../model/Usuario.php";
+    $user = new User();
+    $user->setEmail($correo);
+    $user->setPassword($pw);
     return true;
 }
 
@@ -25,12 +33,56 @@ function buscarUsuarioAPI($correo,$pw){
 // SI existe, consultar y regresar falso
 //Si no existe registrarlo
 function buscaRegistro($nombre, $email, $role, $pw){
-
+    include_once "../model/Usuario.php";
+    $user = new Usuario();
+    if(buscarUsuarioAPI() == true){
+        return false;
+    }
+    else{
+        return $user->creaUser();
+    }
 }
 
 //busca y regresa si true o false si existe o no el correo ya registrado
 function buscarEmailRepetido(){
-
+    include_once "../model/Usuario.php";
+    $user = new User();
+    if(buscarUsuarioAPI()==true){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
+/**
+ * Funcion para crear usuario
+ */
+function creaUser($nombre,$email,$pw,$role,$username){
+    include_once "../model/Usuario.php";
+    $user = new User();
 
+    $user->setNombreUsuario($nombre);
+    $user->setEmail($email);
+    $user->setPassword($pw);
+    $user->setRole($role);
+    $user->setUsername($username);
+
+    return $user->queryRegistrarUsuario();
+}
+
+/**
+ * Funcion para editar usuario
+ */
+function editaUser($nombre,$email,$pw,$role,$username){
+    include_once "../model/Usuario.php";
+    $user = new User();
+
+    $user->setNombreUsuario($nombre);
+    $user->setEmail($email);
+    $user->setPassword($pw);
+    $user->setRole($role);
+    $user->setUsername($username);
+
+    return $user->queryUpdateUsuario();
+}
